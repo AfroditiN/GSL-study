5715bb3d0670a0a71751e689251cf506d0b8c281
AfroditiN <60665434+AfroditiN@users.noreply.github.com>
2021-04-22 14:33:15 +0300

    Add files via upload

--- app.R
+++ app.R
@@ -14,9 +14,9 @@ library(shiny)
 library(jspsychr)
 library(dplyr)
 
-base_dir <- "/srv/shiny-server/GSL-study"
-
-jspsych_dir <- file.path(base_dir, "jspsych-6-2")
+# base_dir <- "/Users/afrod/Desktop/GSL"
+base_dir <- "/Users/au183362/Documents/postdoc/NeDComm/interns/Afroditi_Ntourountzi/GSL"
+jspsych_dir <- file.path(base_dir, "jspsych-6.3.1")
 
 write_to_file <- function(json_object,file_name,var_name=NULL){
   if(is.null(var_name)){
@@ -26,6 +26,9 @@ write_to_file <- function(json_object,file_name,var_name=NULL){
   }
 }
 
+###############################
+# Designing the randomization structure #
+###############################
 # test setup
 real_words = c("αγάπη", "αλλαγή", "βήμα", "βράδυ", "χωριό", "χρώμα", "ευθύνη", "έρευνα", "φύση", "γραμμή", "ιδέα", "λάθος", "λέξη", "λύση", "νίκη", "νησί", "νόμος", "σχέδιο", "όριο", "σελίδα")
 rl = length(real_words)
@@ -40,7 +43,7 @@ lex_test <- data.frame(word = stim,
 video_test = rep(stim[1:(length(stim)/2)], times=4)
 vl = length(video_test)
 vid_stim_test <- data.frame( video = video_test,
-                             video_source = file.path(base_dir, "Final_videos", 
+                             video_source = file.path("videos", 
                                                       c(paste(video_test[1:(vl/2)], 
                                                               "m.mp4", sep = "_"), 
                                                         paste(video_test[(vl/2+1):vl], 
@@ -140,154 +143,61 @@ full_test$stimulus <- html_stimulus(df = full_test,
                                     css = c("color", "font-size"),
                                     id = "id")
 
-##### for testing:
-full_test = full_test[1:5,]
+# ##### for testing:
+# full_test = full_test[1:5,]
 
 # create json object from dataframe
-lex_json <- stimulus_df_to_json(df = full_test,
-                                stimulus = "stimulus",
-                                data = c("word","response","word_type",
-                                         "video","video_source","video_type"))
+# lex_json <- stimulus_df_to_json(df = full_test,
+#                                 stimulus = "stimulus",
+#                                 data = c("word","response","word_type",
+#                                          "video","video_source","video_type"))
 
-# vid_json <- stimulus_df_to_json(df = full_test,
-#                                 stimulus = "video_source",
-                                # data = c("word","response","word_type",
-                                #          "video","stimulus","video_type"))
+vid_json <- stimulus_df_to_json(df = full_test,
+                                stimulus = c("video_source","stimulus"),
+                                data = c("word","response","word_type",
+                                         "video","stimulus","video_type"))
 
 # write json object to script
-write_to_file(lex_json, file.path(base_dir, "lex_stimuli.js"), "lex_stimuli")
-# write_to_file(list(full_test$video_source), file.path(base_dir, "vid_stimuli.js"), "vid_stimuli")
-
+# write_to_file(lex_json, file.path(base_dir, "lex_stimuli.js"), "lex_stimuli")
+write_to_file(vid_json, file.path(base_dir, "vid_stimuli.js"), "vid_stimuli")
 
 ###############################
 ##### jsPsych starts here #####
 ###############################
 
-# # For jsPsych
-
 head <- tags$head(
-  # jsPsych files
-  
-  
-  # # If you want to use original jspsych.js, use this:
-  
   includeScript(file.path(jspsych_dir, "jspsych.js")),
-  
-  # If you want to display text while preloading files (to save time), specify your intro_text
-  # in jsPsych.init (in run-jspsych.js) and call jspsych_preloadprogressbar.js here:
-  #includeScript(file.path(jspsych_dir, "jspsych_preloadprogressbar.js")),
-  
-  includeScript(
-    file.path(jspsych_dir, "plugins/jspsych-html-button-response.js")
-  ),
-  
-  includeScript(
-    file.path(jspsych_dir, "plugins/jspsych-html-keyboard-response.js")
-  ),
-  
-  includeScript(
-    file.path(jspsych_dir, "plugins/jspsych-video-keyboard-response.js")
-  ),
-  
-  # includeScript(
-  #   file.path(jspsych_dir, "plugins/jspsych-fullscreen.js.js")
-  # ),
-  # 
-  includeScript(
-    file.path(jspsych_dir, "plugins/jspsych-survey-text.js")
-  ),
-  
-  includeScript(
-    file.path(jspsych_dir, "plugins/jspsych-survey-multi-choice.js")
-  ),
-  
-  includeScript(
-    file.path(jspsych_dir, "plugins/jspsych-survey-html-form.js")
-  ),
-  
-  includeScript(
-    file.path(jspsych_dir, "plugins/jspsych-instructions.js")
-  ),
-  
-  # includeScript(
-  #   file.path(jspsych_dir, "plugins/jspsych-preload.js")
-  # ),
-  
-  # Custom files
-  includeCSS(file.path(jspsych_dir, "css/jspsych.css"))
-  # includeCSS("css/style.css")
+  includeScript(file.path(jspsych_dir, "plugins/jspsych-html-keyboard-response.js")),
+  includeScript(file.path(jspsych_dir, "plugins/jspsych-html-button-response.js")),
+  includeScript(file.path(jspsych_dir, "plugins/jspsych-video-keyboard-response.js")),
+  includeScript(file.path(jspsych_dir, "plugins/jspsych-survey-text.js")),
+  includeScript(file.path(jspsych_dir, "plugins/jspsych-survey-multi-choice.js")),
+  includeScript(file.path(jspsych_dir, "plugins/jspsych-survey-html-form.js")),
+  includeScript(file.path(jspsych_dir, "plugins/jspsych-instructions.js")),
+  includeScript(file.path(jspsych_dir, "plugins/jspsych-preload.js"))
 )
 
-###################
-
-# PsychTestR elements
-
-#####
-ui_fixa <- tags$div(
-  head,
-  includeScript(file.path(base_dir, "fixa-timeline.js")),
-  includeScript(file.path(base_dir, "run-jspsych.js")),
+ui_video <- tags$div(
+  head, 
+  includeScript(file.path(base_dir, "vid_stimuli.js")),
+  includeScript("video-timeline.js"),
+  includeScript("run-jspsych-vid.js"),
   tags$div(id = "js_psych", style = "min-height: 90vh")
 )
 
-fixation <- page(
-  ui = ui_fixa,
-  label = "fixation",
-  get_answer = function(input, ...)
-    input$jspsych_results,
-  validate = function(answer, ...)
-    nchar(answer) > 0L,
-  save_answer = TRUE
-)
-
-
-ui_video <- tags$video(
-  head,
-  # includeScript(file.path(base_dir, "vid_stimuli.js")),
-  includeScript(file.path(base_dir, "vid-array.js")),
-  includeScript(file.path(base_dir, "video-timeline.js")),
-  includeScript(file.path(base_dir, "run-jspsych.js")),
-  tags$div(id = "js_psych", style = "min-height: 90vh")
-)
-
-video <- page(
+trial <- page(
   ui = ui_video,
-  label = "video",
-  get_answer = function(input, ...)
-    input$jspsych_results,
-  validate = function(answer, ...)
-    nchar(answer) > 0L,
+  label = "trial",
+  get_answer = function(input, ...) input$jspsych_results,
+  validate = function(answer, ...) nchar(answer) > 0L,
   save_answer = TRUE
 )
 
-
-ui_test <- tags$div(
-  head,
-  includeScript(file.path(base_dir, "lex_stimuli.js")),
-  includeScript(file.path(base_dir, "test-timeline.js")),
-  includeScript(file.path(base_dir, "run-jspsych.js")),
-  tags$div(id = "js_psych", style = "min-height: 90vh")
-)
-
-test <- page(
-  ui = ui_test,
-  label = "test",
-  get_answer = function(input, ...)
-    input$jspsych_results,
-  validate = function(answer, ...)
-    nchar(answer) > 0L,
-  save_answer = TRUE
-)
-
-
-########## SURVEY ##########
-
+##### Survey questions #####
 ##Age and nationality
-
 ui_demographics <- tags$div(
   head,
   includeScript(file.path(base_dir, "demographics-timeline.js")),
-  # includeScript(file.path(base_dir, "run-jspsych_full.js")),
   includeScript(file.path(base_dir, "run-jspsych.js")),
   tags$div(id = "js_psych", style = "min-height: 90vh")
 )
@@ -307,7 +217,6 @@ demographics <- page(
 ui_gender <- tags$div(
   head,
   includeScript(file.path(base_dir, "gender-timeline.js")),
-  # includeScript(file.path(base_dir, "run-jspsych_full.js")),
   includeScript(file.path(base_dir, "run-jspsych.js")),
   tags$div(id = "js_psych", style = "min-height: 90vh")
 )
@@ -327,7 +236,6 @@ gender <- page(
 ui_deaf1 <- tags$div(
   head,
   includeScript(file.path(base_dir, "deaf1-timeline.js")),
-  # includeScript(file.path(base_dir, "run-jspsych_full.js")),
   includeScript(file.path(base_dir, "run-jspsych.js")),
   tags$div(id = "js_psych", style = "min-height: 90vh")
 )
@@ -347,7 +255,6 @@ deaf1 <- page(
 ui_deaf2 <- tags$div(
   head,
   includeScript(file.path(base_dir, "deaf2-timeline.js")),
-  # includeScript(file.path(base_dir, "run-jspsych_full.js")),
   includeScript(file.path(base_dir, "run-jspsych.js")),
   tags$div(id = "js_psych", style = "min-height: 90vh")
 )
@@ -367,7 +274,6 @@ deaf2 <- page(
 ui_cochlear1 <- tags$div(
   head,
   includeScript(file.path(base_dir, "cochlear1-timeline.js")),
-  # includeScript(file.path(base_dir, "run-jspsych_full.js")),
   includeScript(file.path(base_dir, "run-jspsych.js")),
   tags$div(id = "js_psych", style = "min-height: 90vh")
 )
@@ -387,7 +293,6 @@ cochlear1 <- page(
 ui_cochlear2 <- tags$div(
   head,
   includeScript(file.path(base_dir, "cochlear2-timeline.js")),
-  # includeScript(file.path(base_dir, "run-jspsych_full.js")),
   includeScript(file.path(base_dir, "run-jspsych.js")),
   tags$div(id = "js_psych", style = "min-height: 90vh")
 )
@@ -407,7 +312,6 @@ cochlear2 <- page(
 ui_mainlang <- tags$div(
   head,
   includeScript(file.path(base_dir, "mainlang-timeline.js")),
-  # includeScript(file.path(base_dir, "run-jspsych_full.js")),
   includeScript(file.path(base_dir, "run-jspsych.js")),
   tags$div(id = "js_psych", style = "min-height: 90vh")
 )
@@ -427,7 +331,6 @@ mainlang <- page(
 ui_neuro <- tags$div(
   head,
   includeScript(file.path(base_dir, "neuro-timeline.js")),
-  # includeScript(file.path(base_dir, "run-jspsych_full.js")),
   includeScript(file.path(base_dir, "run-jspsych.js")),
   tags$div(id = "js_psych", style = "min-height: 90vh")
 )
@@ -442,19 +345,17 @@ neuro <- page(
   save_answer = TRUE
 )
 
-##Thanks
-ui_thanks <- tags$div(
+##Final
+ui_final <- tags$div(
   head,
-  includeScript(file.path(base_dir, "thanks_text.js")),
-  includeScript(file.path(base_dir, "thanks-timeline.js")),
-  # includeScript(file.path(base_dir, "run-jspsych_full.js")),
+  includeScript(file.path(base_dir, "final-timeline.js")),
   includeScript(file.path(base_dir, "run-jspsych.js")),
   tags$div(id = "js_psych", style = "min-height: 90vh")
 )
 
-thanks <- page(
-  ui = ui_thanks,
-  label = "thanks",
+final <- page(
+  ui = ui_final,
+  label = "final",
   get_answer = function(input, ...)
     input$jspsych_results,
   validate = function(answer, ...)
@@ -463,41 +364,32 @@ thanks <- page(
   final = TRUE
 )
 
-
-elts <- join(
-  new_timeline(join(
-    #intro,
-    # demographics,
-    # gender,
-    deaf1,
-    # deaf2,
-    # cochlear1,
-    # cochlear2,
-    # mainlang,
-    # neuro,
-    #education,
-    elt_save_results_to_disk(complete = FALSE), # anything that is saved here counts as completed
-    fixation,
-    video,
-    elt_save_results_to_disk(complete = FALSE), # anything that is saved here counts as completed
-    fixation,
-    test,
-    elt_save_results_to_disk(complete = TRUE), # anything that is saved here counts as completed
-    thanks
-  ), default_lang = "gr")
+elts <- list(
+  # intro,
+  demographics,
+  gender,
+  deaf1,
+  deaf2,
+  cochlear1,
+  cochlear2,
+  mainlang,
+  neuro,
+  education,
+  trial,
+  elt_save_results_to_disk(complete = TRUE),
+  final
 )
 
-exp <- make_test(
+make_test(
   elts = elts,
-  opt = test_options(title="GSL, Aarhus 2021",
-                     admin_password="", # write a secret password here
-                     enable_admin_panel=TRUE,
-                     languages = 'gr',
-                     researcher_email="201902479@post.au.dk",
-                     # problems_info="Problems? Contact 201902479@post.au.dk",
-                     display = display_options(
-                       full_screen = TRUE,
-                       css = file.path(jspsych_dir, "css/jspsych.css")
-                     )))
+  opt = test_options(
+    title="GSL, Aarhus 2021",
+    admin_password="", # write a secret password here
+    enable_admin_panel=TRUE,
+    researcher_email="201902479@post.au.dk",
+    display = display_options(
+      # fullscreen = TRUE, 
+      css = file.path(jspsych_dir, "css/jspsych.css"))
+  ))
 
- # shiny::runApp(".")
+# shiny::runApp(".")
