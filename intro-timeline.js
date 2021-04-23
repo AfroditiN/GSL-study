5c22887628711a47b0f88f01b2cc357e90658e7f
AfroditiN <60665434+AfroditiN@users.noreply.github.com>
2021-04-23 10:42:21 +0300

    Add files via upload

---
+++ intro-timeline.js
@@ -0,0 +1,17 @@
+function new_timeline() {
+    /* defining test timeline*/
+
+    var intro = {
+      timeline: [{
+        type: "html-button-response",
+        choices: ['Συνέχεια'],
+        stimulus: jsPsych.timelineVariable('stimulus'),
+        post_trial_gap: 500
+    }],
+    timeline_variables: intro_text,
+    sample: {type: 'fixed-repetitions', size: 1}
+  };
+  
+  return [intro];
+  }
+  
